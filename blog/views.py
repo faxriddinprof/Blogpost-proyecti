db5e44002c9b3985c2f7b39df5c3ada0f9897b4c
Faxriddin Baxtiyorov <faxriddinbaxtiyorov144@gmail.com>
2025-08-12 14:24:44 +0500

    ...

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,6 @@ from .models import Post
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
